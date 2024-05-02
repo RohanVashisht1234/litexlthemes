b86dd2af153965ec4c9100e594a1906f137a5d57
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-02 14:02:25 +0530

    ok

--- parsers/main.py
+++ parsers/main.py
@@ -1,19 +1,60 @@
 import sys
 from lupa import LuaRuntime
-INITIALIZE_DEFAULT:str = """common = {}
+
+INITIALIZE_DEFAULT: str = """common = {}
 function common.color(color) return color end
 style = {}
 style.syntax = {}
 style.log = {}"""
 
-def sanitize_lua(lines:list) -> str:
+# Default dictionary:
+COLORS: dict = {
+    "normal": "#e1e1e6",
+    "comment": "#676b6f",
+    "literal": "#FFA94D",
+    "string": "#f7c95c",
+    "keyword": "#E58AC9",
+    "keyword2": "#F77483",
+    "function": "#93DDFA",
+    "operator": "#93DDFA",
+    "number": "#FFA94D",
+    "symbol": "#e1e1e6",
+    "dim": "#525257",
+    "warn": "#FFA94D",
+    "scrollbar2": "#4b4b52",
+    "scrollbar": "#414146",
+    "modified": "#1c7c9c",
+    "accent": "#e1e1e6",
+    "line_number": "#525259",
+    "background": "#2e2e32",
+    "error": "#FF3333",
+    "nagbar_text": "#FFFFFF",
+    "good": "#72b886",
+    "background3": "#252529",
+    "nagbar_dim": "rgba(0, 0, 0, 0.45)",
+    "text": "#97979c",
+    "drag_overlay_tab": "#93DDFA",
+    "nagbar": "#FF0000",
+    "selection": "#48484f",
+    "background2": "#252529",
+    "scrollbar_track": "#252529",
+    "drag_overlay": "rgba(255,255,255,0.1)",
+    "caret": "#93DDFA",
+    "divider": "#202024",
+    "line_highlight": "#343438",
+    "line_number2": "#83838f",
+}
+
+
+def sanitize_lua(lines: list) -> str:
     """Sanitize the lua file by removing the lines that have a require in them"""
-    compiled_file:str = ""
+    compiled_file: str = ""
     for line in lines:
         if "require" in line:
             continue
         compiled_file += line
     return compiled_file
+
 
 def read_lua_file(file_name: str) -> list:
     """Read lua file read lines"""
@@ -21,22 +62,25 @@ def read_lua_file(file_name: str) -> list:
         read_lines = fh.readlines()
     return read_lines
 
+
 def main(argc: int, argv: list) -> int:
     """Function main"""
-    lua:any = LuaRuntime(unpack_returned_tuples=True)
+    lua: any = LuaRuntime(unpack_returned_tuples=True)
     lua.execute(INITIALIZE_DEFAULT)
-    sanitized_lua:str = sanitize_lua(read_lua_file(argv[argc]))
+    sanitized_lua: str = sanitize_lua(read_lua_file(argv[argc - 1]))
     lua.execute(sanitized_lua)
-    style:any = lua.globals().style
-    syntax:dict = style.syntax
+    style: any = lua.globals().style
+    syntax: dict = style.syntax
 
     for i in syntax:
-        print(i, syntax[i][1])
+        COLORS[i] = syntax[i][1]
 
     for i in style:
         if i == "syntax" or i == "log":
             continue
         print(i, style[i][1])
+        # colors[i] = syntax[i][1]
+
 
     return 0
 
--- parsers/test.lua
+++ parsers/test.lua
@@ -1,29 +1,37 @@
-local style = require "core.style"
-local common = require "core.common"
+style.background = { common.color "#2e2e32" }  -- Docview
+style.background2 = { common.color "#252529" } -- Treeview
+style.background3 = { common.color "#252529" } -- Command view
+style.text = { common.color "#97979c" }
+style.caret = { common.color "#93DDFA" }
+style.accent = { common.color "#e1e1e6" }
+-- style.dim - text color for nonactive tabs, tabs divider, prefix in log and
+-- search result, hotkeys for context menu and command view
+style.dim = { common.color "#525257" }
+style.divider = { common.color "#202024" } -- Line between nodes
+style.selection = { common.color "#48484f" }
+style.line_number = { common.color "#525259" }
+style.line_number2 = { common.color "#83838f" } -- With cursor
+style.line_highlight = { common.color "#343438" }
+style.scrollbar = { common.color "#414146" }
+style.scrollbar2 = { common.color "#4b4b52" } -- Hovered
+style.scrollbar_track = { common.color "#252529" }
+style.nagbar = { common.color "#FF0000" }
+style.nagbar_text = { common.color "#FFFFFF" }
+style.nagbar_dim = { common.color "rgba(0, 0, 0, 0.45)" }
+style.drag_overlay = { common.color "rgba(255,255,255,0.1)" }
+style.drag_overlay_tab = { common.color "#93DDFA" }
+style.good = { common.color "#72b886" }
+style.warn = { common.color "#FFA94D" }
+style.error = { common.color "#FF3333" }
+style.modified = { common.color "#1c7c9c" }
 
-style.background = { common.color "#24273a" }
-style.background2 = { common.color "#1e2030" }
-style.background3 = { common.color "#363a4f" }
-style.text = { common.color "#cad3f5" }
-style.caret = { common.color "#f4dbd6" }
-style.accent = { common.color "#c6a0f6" }
-style.dim = { common.color "#b8c0e0" }
-style.divider = { common.color "#c6a0f6" }
-style.selection = { common.color "#5b6078" }
-style.line_number = { common.color "#6e738d" }
-style.line_number2 = { common.color "#c6a0f6" }
-style.line_highlight = { common.color "#363a4f" }
-style.scrollbar = { common.color "#5b6078" }
-style.scrollbar2 = { common.color "#6e738d" }
-style.scrollbar_track = { common.color "#1e2030" }
-
-style.syntax["normal"] = { common.color "#ed8796" }
-style.syntax["symbol"] = { common.color "#b7bdf8" }
-style.syntax["comment"] = { common.color "#6e738d" }
-style.syntax["keyword"] = { common.color "#ed8796" }
-style.syntax["keyword2"] = { common.color "#076678" }
-style.syntax["number"] = { common.color "#f5a97f" }
-style.syntax["literal"] = { common.color "#b7bdf8" }
-style.syntax["string"] = { common.color "#a6da95" }
-style.syntax["operator"] = { common.color "#91d7e3" }
-style.syntax["function"] = { common.color "#8aadf4" }
+style.syntax["normal"] = { common.color "#e1e1e6" }
+style.syntax["symbol"] = { common.color "#e1e1e6" }
+style.syntax["comment"] = { common.color "#676b6f" }
+style.syntax["keyword"] = { common.color "#E58AC9" }  -- local function end if case
+style.syntax["keyword2"] = { common.color "#F77483" } -- self int float
+style.syntax["number"] = { common.color "#FFA94D" }
+style.syntax["literal"] = { common.color "#FFA94D" }  -- true false nil
+style.syntax["string"] = { common.color "#f7c95c" }
+style.syntax["operator"] = { common.color "#93DDFA" } -- = + - / < >
+style.syntax["function"] = { common.color "#93DDFA" }
