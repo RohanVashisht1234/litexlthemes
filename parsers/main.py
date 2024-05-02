e22da57c34aeddeffd9c5d199040b459c68f8ea6
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-02 12:20:01 +0530

    ok

--- parsers/main.py
+++ parsers/main.py
@@ -8,45 +8,51 @@ lua.execute("function common.color(color) return color end")
 
 # Define empty style table
 lua.execute("style = {}")
+lua.execute("style.syntax = {}")
+lua.execute("style.log = {}")
 
 # Execute the Lua code
-lua.execute("""
--- GitHub color palette
--- Ported by Andrey Proskurin (proskur1n)
-local bg = { common.color "#22272e" }
-local bg2 = { common.color "#2d333b" }
-local fg = { common.color "#adbac7" }
-local fgdim = { common.color "#768390" }
-local red = { common.color "#f47067" }
-local blue = { common.color "#6cb6ff" }
-local purple = { common.color "#dcbdfb" }
+lua.execute("""style.background = { common.color "#2e2e32" }  -- Docview
+style.background2 = { common.color "#252529" } -- Treeview
+style.background3 = { common.color "#252529" } -- Command view
+style.text = { common.color "#97979c" }
+style.caret = { common.color "#93DDFA" }
+style.accent = { common.color "#e1e1e6" }
+-- style.dim - text color for nonactive tabs, tabs divider, prefix in log and
+-- search result, hotkeys for context menu and command view
+style.dim = { common.color "#525257" }
+style.divider = { common.color "#202024" } -- Line between nodes
+style.selection = { common.color "#48484f" }
+style.line_number = { common.color "#525259" }
+style.line_number2 = { common.color "#83838f" } -- With cursor
+style.line_highlight = { common.color "#343438" }
+style.scrollbar = { common.color "#414146" }
+style.scrollbar2 = { common.color "#4b4b52" } -- Hovered
+style.scrollbar_track = { common.color "#252529" }
+style.nagbar = { common.color "#FF0000" }
+style.nagbar_text = { common.color "#FFFFFF" }
+style.nagbar_dim = { common.color "rgba(0, 0, 0, 0.45)" }
+style.drag_overlay = { common.color "rgba(255,255,255,0.1)" }
+style.drag_overlay_tab = { common.color "#93DDFA" }
+style.good = { common.color "#72b886" }
+style.warn = { common.color "#FFA94D" }
+style.error = { common.color "#FF3333" }
+style.modified = { common.color "#1c7c9c" }
 
-style.background = bg
-style.background2 = bg
-style.background3 = bg
-style.text = fg
-style.caret = red
-style.accent = blue
-style.dim = fgdim
-style.divider = { common.color "#444c56" }
-style.selection = { common.color "#2e4c77" }
-style.line_number = fgdim
-style.line_number2 = fg
-style.line_highlight = bg2
-style.scrol = fgdim
-style.scrollbar2 = fg
+style.syntax["normal"] = { common.color "#e1e1e6" }
+style.syntax["symbol"] = { common.color "#e1e1e6" }
+style.syntax["comment"] = { common.color "#676b6f" }
+style.syntax["keyword"] = { common.color "#E58AC9" }  -- local function end if case
+style.syntax["keyword2"] = { common.color "#F77483" } -- self int float
+style.syntax["number"] = { common.color "#FFA94D" }
+style.syntax["literal"] = { common.color "#FFA94D" }  -- true false nil
+style.syntax["string"] = { common.color "#f7c95c" }
+style.syntax["operator"] = { common.color "#93DDFA" } -- = + - / < >
+style.syntax["function"] = { common.color "#93DDFA" }
 
-style.syntax = {}
-style.syntax["normal"] = fg
-style.syntax["symbol"] = fg
-style.syntax["comment"] = fgdim
-style.syntax["keyword"] = red
-style.syntax["keyword2"] = red
-style.syntax["number"] = blue
-style.syntax["literal"] = blue
-style.syntax["string"] = { common.color "#96d0ff" }
-style.syntax["operator"] = fg
-style.syntax["function"] = blue
+style.log["INFO"]  = { icon = "i", color = style.text }
+style.log["WARN"]  = { icon = "!", color = style.warn }
+style.log["ERROR"] = { icon = "!", color = style.error }
 """)
 
 # Extracted values
@@ -57,6 +63,6 @@ for i in syntax:
     print(i, syntax[i][1])
 
 for i in style:
-    if i == "syntax":
+    if i == "syntax" or i == "log":
         continue
     print(i, style[i][1])
