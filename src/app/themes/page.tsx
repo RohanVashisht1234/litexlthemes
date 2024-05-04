6ed21413eb6ed095733fed2520eb34c84806dd2c
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-05 02:57:09 +0530

    ok

--- src/app/themes/page.tsx
+++ src/app/themes/page.tsx
@@ -10,62 +10,62 @@ function my() {
   let x = document.getElementById("content") as HTMLElement;
   let value = document.getElementById("default-search") as HTMLInputElement;
   let mything = value.value;
-  x.innerHTML = '';
+  x.innerHTML = "";
   for (var i = 0; i < data.addons.length; i++) {
     const addon = data.addons[i];
     if (addon.id.includes(mything)) {
-      x.innerHTML += `<div
-    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:scale-110 transform"
-  >
-    <a href="#">
-      <img
-        class="rounded-t-lg"
-        src=${
-          "https://raw.githubusercontent.com/lite-xl/lite-xl-colors/master/previews/" +
-          addon.id +
-          ".svg"
-        }
-        alt=""
-        style="width:100%;"
-      />
-    </a>
-    <div class="p-5">
-      <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          ${addon.id}
-        </h5>
-      </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-          Version: ${addon.version}
-        </span>
-        <span class="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 capitalize">
-          Type: ${addon.tags[0]}
-        </span>
-      </p>
-      <a
-        href="#"
-        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        View plugin
-        <svg
-          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          />
-        </svg>
-      </a>
-    </div>
-  </div>`;
+      x.innerHTML += (
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:scale-110 transform">
+          <a href="#">
+            <Image
+              className="rounded-t-lg"
+              src={
+                "https://raw.githubusercontent.com/lite-xl/lite-xl-colors/master/previews/" +
+                addon.id +
+                ".svg"
+              }
+              alt=""
+              style={{ width: "100%;" }}
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {addon.id}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                Version: {addon.version}
+              </span>
+              <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300 capitalize">
+                Type: {addon.tags[0]}
+              </span>
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              View plugin
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      );
     }
   }
   return;
@@ -75,7 +75,11 @@ export default async function themes() {
   const data = await getData();
   global_data = data;
   compiled_data = (
-    <div id="content" className="flex flex-wrap justify-center" style={{ gap: "1rem" }}>
+    <div
+      id="content"
+      className="flex flex-wrap justify-center"
+      style={{ gap: "1rem" }}
+    >
       {data.addons.map(
         (addon: { id: string; version: number; tags: string }, index: Key) => (
           <div
@@ -134,7 +138,7 @@ export default async function themes() {
           </div>
         )
       )}
-      </div>
+    </div>
   );
   return (
     <div className="bg-white dark:bg-gray-900">
