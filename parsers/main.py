90423179ba7a90a77a41441abb56779ba13ce0d3
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-05 02:51:25 +0530

    ok

--- parsers/main.py
+++ parsers/main.py
@@ -4,7 +4,7 @@ import json
 import os
 
 
-INPUT_DIR_NAME = './colors'
+INPUT_DIR_NAME = "./colors"
 
 INITIALIZE_DEFAULT: str = """common = {}
 function common.color(color) return color end
@@ -56,6 +56,7 @@ CONSTANT_COLORS: dict = {
 
 COLORS = CONSTANT_COLORS.copy()
 
+
 def sanitize_lua(lines: list) -> str:
     """Sanitize the lua file by removing the lines that have a require in them"""
     compiled_file: str = ""
@@ -72,21 +73,24 @@ def generate_html(file_name: str) -> None:
         OUTPUT_DIR_NAME + "/" + file_name.replace(".lua", ".svg").replace("./", "")
     )
     colors_generated: dict = {}
-    for i in COLORS:
+    for i in COLORS.items():
         colors_generated["--lxl_" + i] = COLORS[i]
     compiled_json: str = json.dumps(colors_generated, indent=4)
 
-    with open(output_file_name, "w") as myfile:
-        with open("./parsers/template.svg", "r") as template_file:
+    with open(output_file_name, "w", encoding="utf-8") as myfile:
+        with open("./parsers/template.svg", "r", encoding="utf-8") as template_file:
             content: str = template_file.read()
-            content = content.replace("{{%INSERT VALUE HERE%}}", compiled_json.replace("\"", "").replace(",", ";"))
+            content = content.replace(
+                "{{%INSERT VALUE HERE%}}",
+                compiled_json.replace('"', "").replace(",", ";"),
+            )
             myfile.write(content)
     return None
 
 
 def read_lua_file(file_name: str) -> list:
     """Read lua file read lines"""
-    with open(file_name, "r") as fh:
+    with open(file_name, "r", encoding="utf-8") as fh:
         read_lines = fh.readlines()
     return read_lines
 
@@ -95,7 +99,7 @@ def main(filename: str) -> int:
     """Function main"""
     lua: any = LuaRuntime(unpack_returned_tuples=True)
     lua.execute(INITIALIZE_DEFAULT)
-    sanitized_lua: str = sanitize_lua(read_lua_file("./colors/"+filename))
+    sanitized_lua: str = sanitize_lua(read_lua_file("./colors/" + filename))
     lua.execute(sanitized_lua)
     style: any = lua.globals().style
     syntax: dict = style.syntax
@@ -118,8 +122,7 @@ def main(filename: str) -> int:
     return 0
 
 
-
-def _start()-> int:
+def _start() -> int:
     for filename in os.listdir(INPUT_DIR_NAME):
         if os.path.isfile(os.path.join(INPUT_DIR_NAME, filename)):
             global COLORS
