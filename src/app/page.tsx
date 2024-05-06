507b07899cd7183a8056d71f7317c39adc992a33
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-06 17:29:14 +0530

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            <span className="bg-gradient-to-r from-orange-400 via-pink-500 to-orange-400 inline-block text-transparent bg-clip-text">View Lite XL</span> <span className="bg-gradient-to-r from-blue-400 via-pink-500 to-indigo-400 inline-block text-transparent bg-clip-text">themes</span>, <span className="bg-gradient-to-r from-teal-500 via-blue-500 to-pink-400 inline-block text-transparent bg-clip-text">Make it look as you like</span>
+            <span className="bg-gradient-to-r from-orange-400 via-pink-500 to-orange-400 inline-block text-transparent bg-clip-text">View Lite XL</span> <span className="bg-gradient-to-r from-blue-400 via-pink-500 to-indigo-400 inline-block text-transparent bg-clip-text">themes,</span> <span className="bg-gradient-to-r from-teal-500 via-blue-500 to-pink-400 inline-block text-transparent bg-clip-text">Make it look as you like</span>
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               Search 50+ Lite XL themes that suits your needs, want to make a
