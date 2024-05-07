ad79cfe45447137f7db9139c4daa8f5163ae74db
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-05-08 02:01:17 +0530

    ok

--- src/app/themes/[slug]/page.tsx
+++ src/app/themes/[slug]/page.tsx
@@ -63,7 +63,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
             <div className="w-full max-w-[16rem] mb-5">
               <div className="relative">
                 <label htmlFor="copy-text" className="sr-only">Label</label>
-                <input id="copy-text" type="text" className="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={"lpm install " + addon_data.id} disabled={false} readonly={false} />
+                <input id="copy-text" type="text" className="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={"lpm install " + addon_data.id} disabled={false} readOnly={false} />
                 <button id="copy-button" className="absolute end-2.5 top-1/2 -translate-y-1/2 text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border">
                   <span id="default-message" className="inline-flex items-center">
                     <svg className="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
