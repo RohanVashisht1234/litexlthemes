8238cb4e49be445f71b6ec4f42890c9dd22dce83
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-09 00:48:41 +0530

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,15 @@ export default function Home() {
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              <span className="bg-gradient-to-r from-orange-400 via-pink-500 to-orange-400 inline-block text-transparent bg-clip-text">View Lite XL</span> <span className="bg-gradient-to-r from-blue-400 via-pink-500 to-indigo-400 inline-block text-transparent bg-clip-text">themes,</span> <span className="bg-gradient-to-r from-teal-500 via-blue-500 to-pink-400 inline-block text-transparent bg-clip-text">Make it look as you like</span>
+              <span className="bg-gradient-to-r from-orange-400 via-pink-500 to-orange-400 inline-block text-transparent bg-clip-text">
+                View Lite XL
+              </span>{" "}
+              <span className="bg-gradient-to-r from-blue-400 via-pink-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+                themes,
+              </span>{" "}
+              <span className="bg-gradient-to-r from-teal-500 via-blue-500 to-pink-400 inline-block text-transparent bg-clip-text">
+                Make it look as you like
+              </span>
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               Search 25+ Lite XL themes that suits your needs, want to make a
@@ -48,51 +56,50 @@ export default function Home() {
             ></Image>
           </div>
         </div>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 dark:bg-gray-900">
-        <br></br>
-        <Carousel
-          slideInterval={1000}
-          className="relative w-full max-w-2xl mx-auto border border-gray-300 shadow-2xl rounded-3xl dark:border-gray-700"
-        >
-          <Image
-            className="w-full h-full p-4"
-            width={1500}
-            height={1500}
-            src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/ayu-mirage.svg"
-            alt="..."
-          />
-          <Image
-            className="w-full h-full p-4"
-            width={1500}
-            height={1500}
-            src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/vscode-dark.svg"
-            alt="..."
-          />
-          <Image
-            className="w-full h-full p-4"
-            width={1500}
-            height={1500}
-            src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/dracula.svg"
-            alt="..."
-          />
-          <Image
-            className="w-full h-full p-4"
-            width={1500}
-            height={1500}
-            src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/catppuccin-macchiato.svg"
-            alt="..."
-          />
-          <Image
-            className="w-full h-full p-4"
-            width={1500}
-            height={1500}
-            src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/monokai.svg"
-            alt="..."
-          />
-        </Carousel>
-      </div>
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 dark:bg-gray-900">
+          <br></br>
+          <Carousel
+            slideInterval={1000}
+            className="relative w-full max-w-2xl mx-auto border border-gray-300 shadow-2xl rounded-3xl dark:border-gray-700"
+          >
+            <Image
+              className="w-full h-full p-4"
+              width={1500}
+              height={1500}
+              src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/ayu-mirage.svg"
+              alt="..."
+            />
+            <Image
+              className="w-full h-full p-4"
+              width={1500}
+              height={1500}
+              src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/vscode-dark.svg"
+              alt="..."
+            />
+            <Image
+              className="w-full h-full p-4"
+              width={1500}
+              height={1500}
+              src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/dracula.svg"
+              alt="..."
+            />
+            <Image
+              className="w-full h-full p-4"
+              width={1500}
+              height={1500}
+              src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/catppuccin-macchiato.svg"
+              alt="..."
+            />
+            <Image
+              className="w-full h-full p-4"
+              width={1500}
+              height={1500}
+              src="https://raw.githubusercontent.com/RohanVashisht1234/litexlthemes/main/parsers/images/monokai.svg"
+              alt="..."
+            />
+          </Carousel>
+        </div>
       </section>
     </>
   );
 }
-
